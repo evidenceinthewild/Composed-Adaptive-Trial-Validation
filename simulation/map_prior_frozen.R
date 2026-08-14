f56971df87556b0a3e646b2307cc500e13f35d9c
Lu Qian <maggie@zetyra.com>
2026-08-14 14:42:57 -0700

    Freeze MAP prior and validate cached simulations

---
+++ simulation/map_prior_frozen.R
@@ -0,0 +1,3 @@
+structure(c(0x1.2d6dccc0e25eap-1, 0x1.2670e74a894adp+4, 0x1.aa2ef8a2b100ep+5,
+0x1.b0af3362dcec2p-3, 0x1.f4d7747f62874p+0, 0x1.2d038da9d93b2p+2,
+0x1.9999999999998p-3, 0x1p+0, 0x1p+0), dim = c(3L, 3L))
